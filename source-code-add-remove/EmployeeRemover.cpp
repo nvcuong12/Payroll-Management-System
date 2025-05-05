986f26a0dbd524ca051841a7892085dadda5f9f3
haison65 <23120162@student.hcmus.edu.vn>
2025-05-05 17:50:39 +0700

    Them va xoa nhan vien

---
+++ source-code-add-remove/EmployeeRemover.cpp
@@ -0,0 +1,15 @@
+﻿#include "Employee.h"
+#include "EmployeeRepository.h"
+
+// Xóa nhân viên khỏi danh sách
+class EmployeeRemover {
+private:
+	EmployeeRepository& empRepo;
+public:
+	EmployeeRemover(EmployeeRepository& repo) : empRepo(repo) {}
+	void removeEmployee(const std::string& empID);
+};
+
+void EmployeeRemover::removeEmployee(const std::string& empID) {
+	empRepo.removeEmployee(empID);
+}
