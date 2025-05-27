badca62ea40c5f3b3271212b3820994f8ef842ae
haison65 <23120162@student.hcmus.edu.vn>
2025-05-28 00:27:27 +0700

    cac chuc nang va cac loai nhan vien

---
+++ source-code/EmployeeAdder.h
@@ -0,0 +1,20 @@
+﻿#ifndef _EMPLOYEE_ADDER_H_
+#define _EMPLOYEE_ADDER_H_
+
+#include "EmployeeManager.h"
+
+/**
+ * @brief Lớp thực hiện chức năng thêm nhân viên vào EmployeeManager.
+ */
+class EmployeeAdder
+{
+public:
+    /**
+     * @brief Thêm nhân viên vào danh sách.
+     * @param manager Tham chiếu tới EmployeeManager.
+     * @param emp unique_ptr tới đối tượng nhân viên cần thêm.
+     */
+    void add(EmployeeManager& manager, std::unique_ptr<IEmployee> emp);
+};
+
+#endif
---
+++ source-code/employeeManager.h
@@ -0,0 +1,40 @@
+#ifndef _EMPLOYEE_MANAGER_H_
+#define _EMPLOYEE_MANAGER_H_
+
+#include <vector>
+#include <memory>
+#include <string>
+#include "IEmployee.h"
+
+/**
+ * @brief Lớp quản lý danh sách nhân viên.
+ *
+ * Lưu trữ danh sách nhân viên, cung cấp các phương thức truy cập dữ liệu.
+ */
+class EmployeeManager
+{
+private:
+    std::vector<std::unique_ptr<IEmployee>> employees;
+
+public:
+    /**
+     * @brief Lấy danh sách con trỏ đến nhân viên.
+     * @return Tham chiếu đến vector chứa unique_ptr nhân viên.
+     */
+    std::vector<std::unique_ptr<IEmployee>>& getEmployees();
+
+    /**
+     * @brief Lấy danh sách nhân viên (chỉ đọc).
+     */
+    const std::vector<std::unique_ptr<IEmployee>>& getEmployees() const;
+
+    /**
+	* @brief Lấy danh sách con trỏ đến nhân viên.
+	* @return Tham chiếu đến vector chứa unique_ptr nhân viên.
+    */
+    std::vector<std::unique_ptr<IEmployee>>& getEmployees() {
+        return employees;
+    }
+};
+
+#endif
